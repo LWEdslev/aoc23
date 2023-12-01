649b8587dc2febb77d3dcd29f33b0f8a30264de3
LWEdslev <202010638@post.au.dk>
2023-12-01 14:10:40 +0100

    aoc01

--- aoc01.f90
+++ aoc01.f90
@@ -1,6 +1,7 @@
-function first_spelled_digit_index(s) result(out)
+subroutine first_spelled_digit(s, out, outi)
   implicit none
-  integer :: out, i, j, length
+  integer, intent(out) :: out, outi
+  integer :: i, j, length
   character(100) :: substring
   character(*), intent(in) :: s
   character(10), dimension(0:9) :: numbers
@@ -14,37 +15,7 @@ function first_spelled_digit_index(s) result(out)
   numbers(7) = 'seven'
   numbers(8) = 'eight'
   numbers(9) = 'nine'
-  out =10000
-  
-  outer: do i = 0, len(s), 1
-    do j = 0, 9, 1
-      length = len(trim(numbers(j)))
-      substring = s(i:(i+length-1))
-      if (trim(substring) == trim(numbers(j))) then
-        out = i
-        exit outer
-      end if
-    end do
-  end do outer
-end function
-
-function first_spelled_digit(s) result(out)
-  implicit none
-  integer :: out, i, j, length
-  character(100) :: substring
-  character(*), intent(in) :: s
-  character(10), dimension(0:9) :: numbers
-  numbers(0) = 'zero'
-  numbers(1) = 'one'
-  numbers(2) = 'two'
-  numbers(3) = 'three'
-  numbers(4) = 'four'
-  numbers(5) = 'five'
-  numbers(6) = 'six'
-  numbers(7) = 'seven'
-  numbers(8) = 'eight'
-  numbers(9) = 'nine'
-  out = 10000
+  outi =10000
   
   outer: do i = 0, len(s), 1
     do j = 0, 9, 1
@@ -52,15 +23,17 @@ function first_spelled_digit(s) result(out)
       substring = s(i:(i+length-1))
       if (trim(substring) == trim(numbers(j))) then
         out = j
+        outi = i
         exit outer
       end if
     end do
   end do outer
-end function
+end subroutine
 
-function last_spelled_digit(s) result(out) 
+subroutine last_spelled_digit(s, out, outi)
   implicit none
-  integer :: out, i, j, length
+  integer, intent(out) :: out, outi
+  integer :: i, j, length
   character(100) :: substring
   character(*), intent(in) :: s
   character(10), dimension(0:9) :: numbers
@@ -75,6 +48,7 @@ function last_spelled_digit(s) result(out)
   numbers(8) = 'eight'
   numbers(9) = 'nine'
   out = -1
+  outi = -1
   
   outer: do i = len(s), 0, -1
     do j = 0, 9, 1
@@ -82,119 +56,55 @@ function last_spelled_digit(s) result(out)
       substring = s(i:(i+length-1))
       if (trim(substring) == trim(numbers(j))) then
         out = j
+        outi = i
         exit outer
       end if
     end do
   end do outer
-end function
+end subroutine
 
-
-function last_spelled_digit_index(s) result(out) 
+subroutine find_first_digit(s, out, outi) 
   implicit none
-  integer :: out, i, j, length
-  character(100) :: substring
-  character(*), intent(in) :: s
-  character(10), dimension(0:9) :: numbers
-  numbers(0) = 'zero'
-  numbers(1) = 'one'
-  numbers(2) = 'two'
-  numbers(3) = 'three'
-  numbers(4) = 'four'
-  numbers(5) = 'five'
-  numbers(6) = 'six'
-  numbers(7) = 'seven'
-  numbers(8) = 'eight'
-  numbers(9) = 'nine'
-  out = -1
-  
-  outer: do i = len(s), 0, -1
-    do j = 0, 9, 1
-      length = len(trim(numbers(j)))
-      substring = s(i:(i+length-1))
-      if (trim(substring) == trim(numbers(j))) then
-        out = i 
-        exit outer
-      end if
-    end do
-  end do outer
-end function
-
-function find_first_digit(s) result(out)
-  implicit none
-  integer :: out
+  integer, intent(out) :: out, outi
   integer :: i, j
   character(*), intent(in) :: s
   character(10) :: digitss
-  out = 10000
+  outi = 10000
   digitss = '0123456789'
   outer: do i = 1, len(s)-1
     do j = 0, 10, 1
       if (s(i:i) == digitss(j:j)) then
         out = j-1
+        outi = i
         exit outer
       end if
     end do
   end do outer
-end function find_first_digit
+end subroutine
 
-function find_first_digit_index(s) result(out)
+subroutine find_last_digit(s, out, outi)
   implicit none
-  integer :: out
+  integer, intent(out) :: out, outi
   integer :: i, j
   character(*), intent(in) :: s
-  character(10) :: digitss
-  out = 10000
-  digitss = '0123456789'
-  outer: do i = 1, len(s)-1
-    do j = 0, 10, 1
-      if (s(i:i) == digitss(j:j)) then
-        out = i
-        exit outer
-      end if
-    end do
-  end do outer
-end function find_first_digit_index
-
-function find_last_digit(s) result(out)
-  implicit none
-  integer :: out, i, j
-  character(*), intent(in) :: s
   character(100) :: digitss
-  out = -1
+  outi = -1
 
   digitss = '0123456789'
   outer: do i = len(s), 0, -1
     do j = 0, 10, 1
       if (s(i:i) == digitss(j:j)) then
-        out = j-1
+        out = j - 1
+        outi = i
         exit outer
       end if
     end do
   end do outer
-end function find_last_digit  
-
-function find_last_digit_index(s) result(out)
-  implicit none
-  integer :: out, i, j
-  character(*), intent(in) :: s
-  character(100) :: digitss
-  out = -1
-
-  digitss = '0123456789'
-  outer: do i = len(s), 0, -1
-    do j = 0, 10, 1
-      if (s(i:i) == digitss(j:j)) then
-        out = i
-        exit outer
-      end if
-    end do
-  end do outer
-end function find_last_digit_index 
+end subroutine
 
 program aoc01
     implicit none
-    integer :: find_first_digit, find_first_digit_index, find_last_digit, find_last_digit_index
-    integer:: last_spelled_digit, last_spelled_digit_index, first_spelled_digit, first_spelled_digit_index
+    
     character(100) :: line
     integer :: unit_number, status, res, sum
     integer :: first_d, first_d_i, last_d, last_d_i, first_w, first_w_i, last_w, last_w_i
@@ -213,14 +123,22 @@ program aoc01
           end if
         end if
         write(*,*) 'Read line: ', trim(line)
-        first_d = find_first_digit(line)
-        first_d_i = find_first_digit_index(line)
-        last_d = find_last_digit(line)
-        last_d_i = find_last_digit_index(line)
-        first_w = first_spelled_digit(line)
-        first_w_i = first_spelled_digit_index(line)
-        last_w = last_spelled_digit(line)
-        last_w_i = last_spelled_digit_index(line)
+
+        call find_first_digit(line, first_d, first_d_i)
+        call find_last_digit(line, last_d, last_d_i)
+
+        call first_spelled_digit(line, first_w, first_w_i)
+        call last_spelled_digit(line, last_w, last_w_i)
+
+        write (*,*) 'fd', first_d
+        write (*,*) 'fdi', first_d_i
+        write (*,*) 'ld', last_d
+        write (*,*) 'ldi', last_d_i
+        write (*,*) 'fw', first_w
+        write (*,*) 'fwi', first_w_i
+        write (*,*) 'lw', last_w
+        write (*,*) 'lwi', last_w_i
+
 
         
         if (first_d_i < first_w_i) then
